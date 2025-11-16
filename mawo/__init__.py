e6c432b1365c0be90562e8315f05089c76835a99
Mawo <xopoiii.slam@gmail.com>
2025-11-16 22:32:28 +1000

    version

--- mawo/__init__.py
+++ mawo/__init__.py
@@ -20,7 +20,7 @@ from .document import Document, Entity, Span, Token
 from .russian import Russian
 from .vocab import CustomVocabulary
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "Russian",
